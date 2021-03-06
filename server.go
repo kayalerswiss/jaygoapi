676301eaaeb1700dbfc2db83856378f3aa647dc6
Nattanon (IT.Appl.SM) <nattanon@corp.jaymart.co.th>
2021-03-06 15:21:04 +0700

    pay bill

--- server.go
+++ server.go
@@ -3,10 +3,21 @@ package main
 import (
 	"net/http"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"os"
 	"log"
 
 )
+
+type Todo struct {
+	id int `json:"id"`
+	Title string `json:"title"`
+	Status string `json:"status"`
+}
+
+var todos = map[int]*Todo{
+	1: &Todo{ID: 1, Title: "pay phone bills", Status:"active"},
+}
 
 func helloHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
@@ -15,10 +26,24 @@ func helloHandler(c echo.Context) error {
 	})
 }
 
+func getTodosHandler(c echo.Context) error {
+	items := []*Todo{}
+	for _, item := range todos {
+		items = append(items, item)
+	}
+
+}
+
 func main() {
 
 	e := echo.New()
+
+	//Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/hello" , helloHandler)
+	e.GET("/todos",getTodosHandler)
 
 	port := os.Getenv("PORT")
 	log.Println("port", port)
